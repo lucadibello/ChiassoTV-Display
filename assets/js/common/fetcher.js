0886b16c71bac592898dd4271669c9f2dc6f6252
Luca Di Bello <luca.dibello@samtrevano.ch>
2020-02-09 20:51:19 +0100

    Done read data from ChiassoTV APIs

--- assets/js/common/fetcher.js
+++ assets/js/common/fetcher.js
@@ -1,1 +1,49 @@
 // Fetcher script
+
+// Global attributes
+var json_config_path = "/config.json"; // Path to config file
+var config; // Object where the config file JSON will be saved
+
+
+// Initial functions calls
+get_json_settings(); // load config
+
+// Read information from ChiassoTV APIs
+function get_videos_information(){
+    $.ajax({
+        dataType: 'json',
+        url: config.api_url,
+        method: "post",
+        data: {
+            token: config.api_token
+        },
+        success: function (data) {
+            console.log("Fetched video information");
+            videos = data;
+            return data;
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.error("Cannot fetch video information from " + config.api_url);
+            console.log(xhr);
+        }
+    });
+}
+
+
+
+// Load config script
+function get_json_settings() {
+    $.ajax({
+        dataType: 'json',
+        url: json_config_path,
+        async: false,
+        success: function (json) {
+            console.log("[Config] Config file read");
+            config = json;
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+        }
+    });
+}
